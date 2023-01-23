5bbaa373ffe5ad2e785f74d885f4a35b80c6350a
Joey <joeymorrison58@gmail.com>
2023-01-23 15:45:00 -0700

    failure

--- lab1/Lab1Joey.py
+++ lab1/Lab1Joey.py
@@ -1,1 +1,21 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-2, 2, 17)
+y = np.linspace(-2, 2, 17)
+xx, yy = np.meshgrid(x,y)
+
+# g = xx*yy
+# h = 1
+# yp = 1/np.sqrt(np.e) * np.e**(x*x/2)
+# g = yy*np.cos(xx)
+# h = 1
+# yp = (np.e**-np.sin(1))*(np.e**np.sin(x))
+g = xx+yy
+h = 1
+yp = (-x-1)+((np.e*np.e**x)/3)
+plt.plot(x, yp)
+plt.ylim(-2,2)
+plt.quiver(xx, yy, h, g)
+
+plt.show()
