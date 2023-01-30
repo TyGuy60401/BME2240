ba52e981f8383a4439c3a4225c19fc212f683164
Ty Davis <tydavisxc2019@gmail.com>
2023-01-30 16:23:09 -0700

    Part 5 finished

--- lab2/lab2TyDoubleTime.py
+++ lab2/lab2TyDoubleTime.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 # We need to do everything twice
 
-# Backwards from 1 to -2
+# Original from 1 to 2
 # Initial Conditions
 x0 = 1
 y0 = 1.5
@@ -11,30 +11,59 @@ slope0 = x0/y0
 
 # Set values for range and step size
 xf = 2
-n = 1001
-deltax = (xf-x0)/(n-1)
+deltax = 0.25
+n = int((xf-x0)/(deltax) + 1)
 
 # Create array for all x values
-x = np.linspace(x0, xf, n)
+xright = np.linspace(x0, xf, n)
 
 # Create some arrays full of zeros
 # for later use
-y = np.zeros([n])
-slope = np.zeros([n])
+yright = np.zeros([n])
+sloperight = np.zeros([n])
 
 # Set the first value to our initial
 # conditons
-y[0] = y0
-slope[0] = slope0
+yright[0] = y0
+sloperight[0] = slope0
 
 # Create loop to calculate next
 # y-value based on previous coord
 for i in range(1, n):
-    y[i] = deltax*(x[i-1]/y[i-1]) + y[i-1]
+    yright[i] = deltax*(xright[i-1]/yright[i-1]) + yright[i-1]
+
+
+# Backwards from 1 to -2
+x0 = 1
+y0 = 1.5
+slope0 = x0/y0
+
+xf = -2
+deltax = 0.25
+n = int((x0-xf)/(deltax)+1)
+
+xleft = np.linspace(x0, xf, n)
+
+yleft = np.zeros([n])
+slopeleft = np.zeros([n])
+
+yleft[0] = y0
+slopeleft[0] = slope0
+
+for i in range(1, n):
+    yleft[i] = -deltax*(xleft[i-1]/yleft[i-1]) + yleft[i-1]
+
+# x = xleft + xrightt
+x = np.append(xleft, xright)
+# y = yleft + yright
+y = np.append(yleft, yright)
+
+xx = np.linspace(-2, 2, 1001)
 
 # Now plot it
 plt.plot(x, y, '.')
+plt.plot(xx, np.sqrt(xx**2+1.25), c="green")
 plt.xlabel("x-axis")
 plt.ylabel("y-axis")
-plt.title("Solution of Differential Equation through y(-2)=1")
+plt.title("Solution of Differential Equation through y(2)=1.5")
 plt.show()
