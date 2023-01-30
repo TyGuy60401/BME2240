fb5721d8a70b8e3287bc868d06f60e4cb40a11c6
Ty Davis <tydavisxc2019@gmail.com>
2023-01-30 14:47:11 -0700

    Lab 2 Ty working

--- lab2/lab2Ty.py
+++ lab2/lab2Ty.py
@@ -2,7 +2,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-fout = open('table.csv', 'w')
+foutname = input("File name: ")
+fout = open(foutname, 'w')
 
 # Initial Conditions
 x0 = -2
@@ -13,8 +14,8 @@ slope0 = x0*x0*y0-x0*y0
 xf = 2
 # n = 1001
 # deltax = (xf-x0)/(n-1)
-deltax = 0.2
-n = 21
+deltax = float(input("Delta x: "))
+n = int((xf-x0)/(deltax) + 1)
 
 # Create array for all x values
 x = np.linspace(x0, xf, n)
@@ -29,14 +30,16 @@ slope = np.zeros([n])
 y[0] = y0
 slope[0] = slope0
 
+
+
 # Create loop to calculate next
 # y-value based on previous coord
 fout.write('x,y,dy/dx\n')
-fout.write('%.1f,%.3f,%.3f\n' % (x0, y0, slope0))
+fout.write('%.3f,%.3f,%.3f\n' % (x0, y0, slope0))
 for i in range(1, n):
     y[i] = deltax*(x[i-1]**2*y[i-1]-x[i-1]*y[i-1]) + y[i-1]
     slope = (x[i]**2*y[i]-x[i]*y[i]) 
-    fout.write('%.1f,%.3f,%.3f\n' % (x0+i*deltax, y[i], slope))
+    fout.write('%.3f,%.3f,%.3f\n' % (x0+i*deltax, y[i], slope))
 
 # Now plot it
 plt.plot(x, y, '.')
