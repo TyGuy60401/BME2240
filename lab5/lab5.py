f6dddb151e063cb58e148edac69b6ea0d3a1036d
Cole <zacharyromero@mail.weber.edu>
2023-03-20 16:09:47 -0600

    Finished lab 5

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -2,22 +2,43 @@ import matplotlib.pyplot as plt
 from scipy import signal
 
 def main():
-    """Plotting a transfer function, and displaying the zeroes and poles with x and o """
     res = 470
     cap = 0.1E-6
     RC = res*cap
     A = 0
     B = 0
-    C = 5
-    D = RC
-    E = 1
-    F = 0
+    C = 1
+    D = 0
+    E = RC
+    F = 1
     sys = signal.TransferFunction([A,B,C],[D,E,F])
     w, mag, phase = signal.bode(sys)
     plt.figure()
     plt.semilogx(w,mag) #magnitude
+    plt.title("Magnitude of RC Circuit")
     plt.figure()
     plt.semilogx(w,phase) #phase
+    plt.title("Phase Angle of RC Circuit")
+
+    res = 470
+    cap = 0.01E-6
+    induc = 100E-3
+    RC = res*cap
+    LC = induc*cap
+    A = 0
+    B = 0
+    C = 1
+    D = LC
+    E = RC
+    F = 1
+    sys = signal.TransferFunction([A,B,C],[D,E,F])
+    w, mag, phase = signal.bode(sys)
+    plt.figure()
+    plt.semilogx(w,mag) #magnitude
+    plt.title("Magnitude of RLC Circuit")
+    plt.figure()
+    plt.semilogx(w,phase) #phase
+    plt.title("Phase Angle of RLC Circuit")
     plt.show()
 
 if __name__=="__main__":
