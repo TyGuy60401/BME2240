92e938d98187f6c9d3d0ddaf5d7c17fa1c3fbd5f
Ty Davis <tydavisxc2019@gmail.com>
2023-04-23 20:47:33 -0600

    Got qrgs working

--- final/linear_algebra.py
+++ final/linear_algebra.py
@@ -2,13 +2,14 @@ import numpy as np
 import sys
 
 def main(argv):
+    v1 = np.array([1, 2, 3])
+    v2 = np.array([[4], [5], [6]])
     V = np.array([1, 0, 1])
     # print(normalize(V))
     a = np.array([[1, 1, 2],
                   [0, -1, 2],
                   [1, -1, 1]])
-    print(get_e(a, 1))
-    # print(np.subtract(a, 1))
+    Q, R = qrgs(a)
     return
 
 def qrgs(A):
@@ -28,8 +29,21 @@ def qrgs(A):
         upper triangular matrix R size NxN"""
     
     # We need to calculate the Q, might use some recursion 👀
+    Q = np.zeros(A.shape)
+    for i in range(len(A[0])):
+        Q = np.concatenate((Q[:,0:i], get_e(A, i+1), Q[:,i+1:]), axis=1)
+    
+    # Making the R matrix
+    n = A.shape[1]
+    R = np.zeros((n,n))
+    for i in range(len(R)):
+        for j in range(len(R[i])):
+            if j >= i:
+                v1 = np.squeeze(A[:,j:j+1])
+                v2 = np.squeeze(get_e(A, i+1))
+                R[i,j] = np.dot(v1, v2)
 
-    return
+    return Q, R
 
 def normalize(V):
     """Normalize a vector V to its unit vector."""
@@ -37,7 +51,7 @@ def normalize(V):
     # Calculate the length
     for val in V:
         radicand += val*val
-    print(np.abs(V))
+    # print(np.abs(V))
     length = np.sqrt(radicand)
     return V/length
 
@@ -48,12 +62,20 @@ def get_e(A, n):
     # un = an - (dot(an, en-1))*en-1 - (dot(an,en-2))*en-2 - ... - (dot(an,e1))*e1
     if n == 1:
         u = A[:,0:1]
-        e = normalize(u)
-        return e
     elif n > 1:
         an = A[:, n-1:n]
-        # subtrahend = 
-        # u =  
+        u = an
+        while n > 1:
+            en_1 = get_e(A, n-1)
+            v1 = np.squeeze(an)
+            v2 = np.squeeze(en_1)
+            val = np.dot(v1, v2)
+            
+            u = np.subtract(u, val * en_1)
+            n -= 1
+    
+    e = normalize(u)
+    return e
 
 
 if __name__ == '__main__':
